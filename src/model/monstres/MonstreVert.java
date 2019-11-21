9a9daae7bae620db41ac34d74414d4f54829b508
ileft <ileft@192.168.1.100>
2019-11-21 20:56:00 +0100

    draw monsters

--- src/model/monstres/MonstreVert.java
+++ src/model/monstres/MonstreVert.java
@@ -4,18 +4,35 @@ import model.Constantes;
 import model.Labyrinthe;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 public class MonstreVert extends Monstre {
-
+public TexturePaint monstre;
     public MonstreVert(int x, int y, Labyrinthe labyrinthe) {
         super(x, y, labyrinthe);
     }
 
     public void afficher(Graphics2D g) {
-
-        g.setColor(Color.GREEN);
-        g.fillOval(x * Constantes.tailleCase, y * Constantes.tailleCase, Constantes.tailleCase, Constantes.tailleCase);
-
+         BufferedImage buffer = null;
+         Path path = FileSystems.getDefault().getPath(".").toAbsolutePath();
+        try {
+            buffer = ImageIO.read(new File(path.toString()+"/src/model/images/monstrevert.png"));            
+        } catch (IOException ex) {
+            Logger.getLogger(MonstreVert.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      
+        // g.setColor(Color.GREEN);
+        //g.fillOval(x * Constantes.tailleCase, y * Constantes.tailleCase, Constantes.tailleCase, Constantes.tailleCase);
+         g.drawImage(buffer, x * Constantes.tailleCase, y * Constantes.tailleCase,Constantes.tailleCase,Constantes.tailleCase,null);
+                
     }
 
 
